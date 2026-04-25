9a9dfbdd304469f16426002b9ac0acdc5fa2911c
youssef <majdoubyoussef79@gmail.com>
2026-04-25 20:10:41 +0100

    adding logo and minor debugging

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-    title: 'uCAR | University Interface',
+    title: 'UCAR | University Interface',
     year: '2026' 
   });
 });
